6020798b087901dbe108b21f1e869f8ca95a63f9
Shreyansh Arora <shreyansharora69@gmail.com>
2024-09-08 01:35:36 +0530

    updates

--- .next/types/app/layout.ts
+++ .next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: E:\My projects\Codenex\CODENEX-RECURTMENT-24-main\src\app\layout.tsx
+// File: E:\My projects\Codenex site\CODENEX-RECURTMENT-24\src\app\layout.tsx
 import * as entry from '../../../src/app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
--- .next/types/app/page.ts
+++ .next/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: E:\My projects\Codenex\CODENEX-RECURTMENT-24-main\src\app\page.tsx
+// File: E:\My projects\Codenex site\CODENEX-RECURTMENT-24\src\app\page.tsx
 import * as entry from '../../../src/app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
